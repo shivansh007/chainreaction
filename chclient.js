2159b20c2b6eea2124cf3ac6a71c30b9937a5717
thomas154 <blessonthomas154@gmail.com>
2016-10-12 01:09:36 +0530

    files added

---
+++ chclient.js
@@ -0,0 +1,107 @@
+var num=[[0,0,0,0,0,0],[0,0,0,0,0,0],[0,0,0,0,0,0],[0,0,0,0,0,0],[0,0,0,0,0,0],[0,0,0,0,0,0],[0,0,0,0,0,0],[0,0,0,0,0,0],[0,0,0,0,0,0]];
+var ws;
+
+			var name=prompt("enter your name");
+			function checkgame(x,y)
+			{	var arr=[x,y,name];
+				jarr=JSON.stringify(arr);
+				ws.send(jarr);
+			}
+			function initiate(arr)
+			{	jdec=JSON.parse(arr);
+				var x=jdec[0];
+				var y=jdec[1];
+				num[x][y]++;
+				var pt=""+x+y;
+				document.getElementById(pt).innerHTML=num[x][y];
+				check(x,y,num[x][y]);
+			}
+
+			function check(x,y,value)
+			{
+				if(value==2)
+				{
+					if((x==0&&y==0) || (x==8&&y==0) || (x==0&&y==5) || (x==8&&y==5))
+						brkblk(x,y);
+				}
+				if(value==3)
+				{
+					if((x==0) || (x==8 || (y==0) || (y==5)))
+						brkblk(x,y);
+				}
+				if(value==4)
+				{
+						brkblk(x,y);
+				}
+				draw(x,y);
+			}
+			function brkblk(x,y)
+			{
+				num[x][y]=0;
+				if(x!=0)
+				{
+					num[x-1][y]++;
+					check(x-1,y,num[x-1][y]);
+				}
+				if(x!=8)
+				{
+					num[x+1][y]++;
+					check(x+1,y,num[x+1][y]);
+				}
+				if(y!=5)
+				{
+					num[x][y+1]++;
+					check(x,y+1,num[x][y+1]);
+				}
+				if(y!=0)
+				{
+					num[x][y-1]++;
+					check(x,y-1,num[x][y-1]);
+				}
+			}
+			function draw(x,y)
+			{
+				var pt=""+x+y;
+				if(num[x][y]==0)
+					document.getElementById(pt).innerHTML="";
+				if(num[x][y]==1)
+					document.getElementById(pt).innerHTML="<img src='redSingle.png' width=45px height=45px/>";
+				if(num[x][y]==2)
+					document.getElementById(pt).innerHTML="<img src='redDouble.png' width=45px height=45px />";
+				if(num[x][y]==3)
+					document.getElementById(pt).innerHTML="<img src='redTriple.png' width=45px height=45px />";
+
+			}
+
+
+
+function setupChat(){
+	var HOST=location.origin.replace(/^http/,'ws');
+	//HOST="wss://chainreaction154.herokuapp.com:443/";
+	ws=new WebSocket(HOST);
+	write("welcome to very simple chat");
+	ws.addEventListener("open",function(){
+		write("opened connection");
+	},false);
+	ws.addEventListener("message",function(e){
+		initiate(e.data);
+	},false);
+	ws.addEventListener("close",function(){
+		write("connection closed");
+	},false);
+}
+
+window.addEventListener("load",setupChat,false);
+
+/*function setupInput(pt){
+	var input = document.getElementById(pt);
+	input.addEventListener("keydown",function(e){
+		if(e.keyCode==13){
+			ws.send(this.value);
+			this.value="";
+		}
+	});
+}*/
+function write(str){
+	//var response = document.getElementById("response"),
+	console.log(str);}
---
+++ chserver.js
@@ -0,0 +1,66 @@
+var players=2;
+var express=require('express');
+var app=express();
+port=process.env.PORT || 3000;
+//port=443;
+app.use(express.static(__dirname));
+app.get("/",function(req,res){
+	res.render("index.html");
+})
+var webSocketServer=require("websocket").server;
+var server= require("https").createServer(app);
+server.on("connection",function(socket){
+	console.log("connection from: ",socket.address().address);});
+var ws=new webSocketServer({
+	httpServer :server,
+	autoAcceptConnections: true
+	});
+console.log(port);
+server.listen(port);
+
+var clients=[];
+ws.on("connect" , connectHandler);
+function connectHandler(conn){
+	conn.nickname=conn.remoteAddress;
+	conn.on("message", messageHandler);
+	conn.on("close", closeHandler);
+	if(clients.length<players)
+		{clients.push(conn);
+		console.log(conn.nickname + "entered the game");}
+	else
+		{console.log("overlimit");
+		closeHandler();
+		}
+
+}
+
+function broadcast(data){
+	clients.forEach(function(client){
+		client.sendUTF(data);
+	});
+}
+function closeHandler(){
+	var index=clients.indexOf(this);
+	if(index>-1){
+		clients.splice(index,1);
+	}
+		console.log(this.nickname + "left the game");
+}
+
+function messageHandler(message){
+	var data=JSON.parse(message.utf8Data);
+	this.nickname=data[2];
+	data=JSON.stringify(data);
+	console.log(this.nickname +"  "+data);
+	broadcast(data);
+}
+/*function game_loop(){
+
+	while(1)
+	{
+		for(i=0;i<players;i++)
+		{
+
+		}
+	}
+}*/
