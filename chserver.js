8c1377ea2be21ccd67338aa8b46aa4bc67d73c98
thomas154 <blessonthomas154@gmail.com>
2016-10-12 22:57:26 +0530

    changes

--- chserver.js
+++ chserver.js
@@ -5,7 +5,7 @@ port=process.env.PORT ||3000;
 //port=443;
 app.use(express.static(__dirname));
 app.get("/",function(req,res){
-	res.render("index.html");
+	res.render("index.php");
 })
 var webSocketServer=require("websocket").server;
 var server= require("http").createServer(app);
